62d72ac1492b0e4b0b301dd33fbc31bf0ce9e947
takuya-nakayasu <l08084.1989@gmail.com>
2025-02-22 20:04:53 +0900

    base 変更

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/spa">Home</RouterLink>
-        <RouterLink to="/spa/about">About</RouterLink>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
